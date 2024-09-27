14c776b67e5e181e40429e5d9e47303dc213690a
gregory <gregorypinheiro88@gmail.com>
2024-09-27 15:59:13 -0300

    exercício finalizado

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
                     style: 'compressed'
                 },
                 files: {
-                    'build/styles/main.css': 'src/styles/main.scss'
+                    'dist/styles/main.css': 'src/styles/main.scss'
                 }
             }
         },
